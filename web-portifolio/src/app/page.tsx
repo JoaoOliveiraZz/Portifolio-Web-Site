8de8ce3b2cbde1ea0572d5c9cd5409ad0de6b820
JoaoOliveiraZz <linajoaovictor@gmail.com>
2023-05-24 21:14:07 -0300

    :tada: Initial commit

---
+++ web-portifolio/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { PartyPopper } from 'lucide-react'
+
+export default function Home() {
+  return (
+    <main>
+      <h1>
+        My initial web portifolio <PartyPopper />
+      </h1>
+    </main>
+  )
+}
